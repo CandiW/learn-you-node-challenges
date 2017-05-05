929bc90d0c5cca0a6ae5544a8fdf808beabc0943
CandiW <candisw06@gmail.com>
2017-05-05 07:48:46 -0700

    solution for Time Server

---
+++ time-server.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+function zerofill(num){
+    if(num < 10 || num === 0 || num === ""){
+        return "0" + num;
+    }
+}
+
+function getDate(){
+    let d = new Date();
+    return d.getFullYear() + "-" + zerofill(d.getMonth()+1) + "-" + zerofill(d.getDate()) + " " + zerofill(d.getHours()) + ":" + d.getMinutes();
+}
+
+function timeserver(port){
+    let server = net.createServer(function (socket){
+        socket.end(getDate() + "\n");
+    });
+    server.listen(port);
+}
+
+timeserver(process.argv[2]);
