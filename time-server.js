339f50120727aacf85abbf6c3319fdf3c2f78b94
CandiW <candisw06@gmail.com>
2017-05-05 07:54:03 -0700

    modified solution for Time Server

--- time-server.js
+++ time-server.js
@@ -4,11 +4,14 @@ function zerofill(num){
     if(num < 10 || num === 0 || num === ""){
         return "0" + num;
     }
+    else {
+        return num;
+    }
 }
 
 function getDate(){
     let d = new Date();
-    return d.getFullYear() + "-" + zerofill(d.getMonth()+1) + "-" + zerofill(d.getDate()) + " " + zerofill(d.getHours()) + ":" + d.getMinutes();
+    return d.getFullYear() + "-" + zerofill(d.getMonth()+1) + "-" + zerofill(d.getDate()) + " " + zerofill(d.getHours()) + ":" + zerofill(d.getMinutes());
 }
 
 function timeserver(port){
