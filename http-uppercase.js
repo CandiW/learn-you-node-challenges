0f8e211193bb44036a16d066c7ae3bd86ca6c935
CandiW <candisw06@gmail.com>
2017-05-05 10:22:36 -0700

    Solution for HTTP Uppercaser

---
+++ http-uppercase.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const map = require('through2-map');
+
+function uppercase(port){
+
+    let server = http.createServer(function(req, resp){
+
+        if(req.method !== 'POST'){
+            return res.end('Send POST');
+        }
+        else {
+        req.pipe(map(function(chunk){
+            return chunk.toString().toUpperCase();
+        })).pipe(resp);
+        }
+    });
+    server.listen(port);
+}
+
+uppercase(process.argv[2]);
